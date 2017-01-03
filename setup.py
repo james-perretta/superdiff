2add4dc44d4c3d09da40b32a73c32b58723b864d
James P <jameslp@umich.edu>
2017-01-02 23:30:21 -0500

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #! /usr/bin/env python3
 
+
 from setuptools import setup
 
 setup(name='superdiff',
-      version='1.0.0a0',
-      description="Extends Python's difflib to allow more flexible diffing",
+      version='1.0.0',
+      description='A library for performing flexible diff checks.',
       author='James Perretta',
       author_email='jameslp@umich.edu',
-      packages=["superdiff"],
-      install_requires=[])
+      license='GNU Lesser General Public License v3',
+      url='https://github.com/james-perretta/superdiff',
+      packages=['superdiff'],
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+      ])
